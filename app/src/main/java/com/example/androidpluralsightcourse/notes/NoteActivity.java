1d7238aa652bff99c9555450e67ce700b00d32aa
eman <emankamalnasr96@gmail.com>
2022-08-26 22:46:49 +0200

    send_note_as_an_email

--- app/src/main/java/com/example/androidpluralsightcourse/notes/NoteActivity.java
+++ app/src/main/java/com/example/androidpluralsightcourse/notes/NoteActivity.java
@@ -25,22 +25,28 @@ public class NoteActivity extends AppCompatActivity {
     public static final String NOTE_INFO = "com.example.androidpluralsightcourse.notes.data.NOTE_INFO";
     private NoteInfo mNote;
     private boolean mIsNewNote;
+    private EditText textNoteTitle;
+    private EditText textNoteText;
+    private Spinner spinnerCourses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-        EditText textNoteTitle = findViewById(R.id.text_note_title);
-        EditText textNoteText = findViewById(R.id.text_note_text);
-        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
-
+        setUpViews();
         setUpCustomToolBar();
-        initCoursesSpinner(spinnerCourses);
+        initCoursesSpinner();
         readDisplayStateValues();
 
-        if(!mIsNewNote) displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        if(!mIsNewNote) displayNote();
 
 
+    }
+
+    private void setUpViews(){
+        textNoteTitle = findViewById(R.id.text_note_title);
+        textNoteText = findViewById(R.id.text_note_text);
+        spinnerCourses = findViewById(R.id.spinner_courses);
     }
 
     private void setUpCustomToolBar(){
@@ -48,15 +54,15 @@ public class NoteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    private void initCoursesSpinner(Spinner spinner){
+    private void initCoursesSpinner(){
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         ArrayAdapter<CourseInfo> adapterCourses =
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapterCourses);
+        spinnerCourses.setAdapter(adapterCourses);
     }
 
-    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+    private void displayNote() {
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         int courseIndex = courses.indexOf(mNote.getCourse());
         spinnerCourses.setSelection(courseIndex);
@@ -82,9 +88,22 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_send_email) {
+            sendEmail();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendEmail() {
+        CourseInfo course = (CourseInfo) spinnerCourses.getSelectedItem();
+        String subject = textNoteTitle.getText().toString();
+        String text = "Checkout what I learned in the Pluralsight course \"" +
+                course.getTitle() + "\"\n" + textNoteText.getText();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
     }
 }
