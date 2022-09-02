798fc67b702333772bbe09d405fb8e3e11be1e9c
eman <emankamalnasr96@gmail.com>
2022-09-03 00:11:14 +0200

    settings_screen_logic_pref

--- app/src/main/java/com/example/androidpluralsightcourse/notes/views/MainActivity.java
+++ app/src/main/java/com/example/androidpluralsightcourse/notes/views/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.androidpluralsightcourse.notes.views;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -11,6 +14,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,6 +27,7 @@ import com.example.androidpluralsightcourse.notes.data.CourseInfo;
 import com.example.androidpluralsightcourse.notes.data.DataManager;
 import com.example.androidpluralsightcourse.notes.data.NoteInfo;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setPrefDefaultValues();
         displayNotes();
         navigateToCreateNewNote();
         setUpSupportActionBar();
@@ -45,10 +51,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    private void setPrefDefaultValues(){
+        PreferenceManager.setDefaultValues(this,R.xml.general_preferences,false);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         notesAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navView = binding.navView;
+        View header = navView.getHeaderView(0);
+        TextView username = header.findViewById(R.id.usernameTextView);
+        TextView email = header.findViewById(R.id.emailTextView);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String usernamePrefKey = getString(R.string.user_key);
+        String emailPrefKey = getString(R.string.email_key);
+        username.setText(pref.getString(usernamePrefKey,""));
+        email.setText(pref.getString(emailPrefKey,""));
     }
 
     private void navigateToCreateNewNote(){
@@ -89,6 +112,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_settings){
+            startActivity(new Intent(MainActivity.this,SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id){
@@ -98,6 +130,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.menu_notes_item:
                 displayNotes();
                 break;
+            case R.id.menu_share:
+                share();
+                break;
             default:
                 break;
         }
@@ -105,6 +140,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    private void share() {
+        String favoriteSocialMedia = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.social_key),"twitter");
+        showSnackBar("share to "+favoriteSocialMedia);
+    }
+
+    private void showSnackBar(String message){
+        View root = binding.appBarMain.mainRoot;
+        Snackbar.make(root,message,Snackbar.LENGTH_LONG).show();
+    }
     @Override
     public void onBackPressed() {
         if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
