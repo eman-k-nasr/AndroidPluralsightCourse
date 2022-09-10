b71d03efc1a6932fd354c1c87788a1413bca4198
eman <emankamalnasr96@gmail.com>
2022-09-10 11:25:22 +0200

    create_new_note

--- app/src/main/java/com/example/androidpluralsightcourse/notes/views/NoteActivity.java
+++ app/src/main/java/com/example/androidpluralsightcourse/notes/views/NoteActivity.java
@@ -108,8 +108,13 @@ public class NoteActivity extends AppCompatActivity
     }
 
     private void createNewNote() {
-        mNoteID = DataManager.getInstance().createNewNote();
-//        mNote = getNoteByPosition(mNoteID);
+        ContentValues values = new ContentValues();
+        values.put(NoteInfoEntry.COLUMN_COURSE_ID, "");
+        values.put(NoteInfoEntry.COLUMN_NOTE_TITLE, "");
+        values.put(NoteInfoEntry.COLUMN_NOTE_TEXT, "");
+
+        SQLiteDatabase db = mDbOpenHelper.getWritableDatabase();
+        mNoteID = (int) db.insert(NoteInfoEntry.TABLE_NAME, null, values);
     }
 
     @Override
